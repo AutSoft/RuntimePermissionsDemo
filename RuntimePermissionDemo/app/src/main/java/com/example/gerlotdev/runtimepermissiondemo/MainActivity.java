e45d608d7ea0471b95a2c8855f14c5ebee85615b
Balázs Gerlei <gerlei.balazs@autsoft.hu>
2016-10-13 12:30:15 +0200

    - More style fixes - Added dialog with dangerous permissions - Removed menu from the MainActvity

--- RuntimePermissionDemo/app/src/main/java/com/example/gerlotdev/runtimepermissiondemo/MainActivity.java
+++ RuntimePermissionDemo/app/src/main/java/com/example/gerlotdev/runtimepermissiondemo/MainActivity.java
@@ -2,12 +2,10 @@ package com.example.gerlotdev.runtimepermissiondemo;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,32 +20,12 @@ public class MainActivity extends AppCompatActivity {
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+				new AlertDialog.Builder(MainActivity.this)
+						.setTitle(R.string.dangerous_permissions)
+						.setItems(R.array.dangerous_permissions, null)
+						.show();
 			}
 		});
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
 	}
 
 }
