78daada234b73e81c755eb04a78edb701ee42e01
Balázs Gerlei <gerlei.balazs@autsoft.hu>
2016-10-13 13:12:04 +0200

    - Added Google Play Services and started implementing the real behaviour

--- RuntimePermissionDemo/app/src/main/java/com/example/gerlotdev/runtimepermissiondemo/MainActivity.java
+++ RuntimePermissionDemo/app/src/main/java/com/example/gerlotdev/runtimepermissiondemo/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.gerlotdev.runtimepermissiondemo;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+	private Button buttonDoYourThing;
+	private Button buttonDoWhatever;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,29 @@ public class MainActivity extends AppCompatActivity {
 						.show();
 			}
 		});
+
+		buttonDoYourThing = (Button) findViewById(R.id.buttonDoYourThing);
+		buttonDoYourThing.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showMessage(getString(R.string.do_your_thing));
+			}
+		});
+
+		buttonDoWhatever = (Button) findViewById(R.id.buttonDoWhatever);
+		buttonDoWhatever.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showMessage(getString(R.string.do_whatever));
+			}
+		});
+
+	}
+
+	private void showMessage(String message) {
+		Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG);
+		snackbar.getView().setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null));
+		snackbar.show();
 	}
 
 }
