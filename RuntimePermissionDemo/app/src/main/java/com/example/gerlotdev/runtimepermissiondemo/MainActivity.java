c5e5579c396ce690904235a03e9aae7786db9ceb
Balázs Gerlei <gerlei.balazs@autsoft.hu>
2016-10-13 13:22:13 +0200

    - Added getting the users last known location to Do Your Thing Click

--- RuntimePermissionDemo/app/src/main/java/com/example/gerlotdev/runtimepermissiondemo/MainActivity.java
+++ RuntimePermissionDemo/app/src/main/java/com/example/gerlotdev/runtimepermissiondemo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gerlotdev.runtimepermissiondemo;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.res.ResourcesCompat;
@@ -10,7 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+	private GoogleApiClient googleApiClient;
 
 	private Button buttonDoYourThing;
 	private Button buttonDoWhatever;
@@ -21,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
+
+		if (googleApiClient == null) {
+			googleApiClient = new GoogleApiClient.Builder(this)
+					.addConnectionCallbacks(this)
+					.addOnConnectionFailedListener(this)
+					.addApi(LocationServices.API)
+					.build();
+		}
 
 		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 		fab.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
 			@Override
 			public void onClick(View v) {
 				showMessage(getString(R.string.do_your_thing));
+				googleApiClient.connect();
 			}
 		});
 
@@ -50,6 +68,34 @@ public class MainActivity extends AppCompatActivity {
 		});
 
 	}
+
+	@Override
+	protected void onDestroy() {
+		googleApiClient.disconnect();
+		super.onDestroy();
+	}
+
+	@Override
+	public void onConnected(@Nullable Bundle bundle) {
+		Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(
+				googleApiClient);
+		if (lastLocation != null) {
+			showMessage("Lat: " + lastLocation.getLatitude() + ", Lon: " + lastLocation.getLongitude());
+			googleApiClient.disconnect();
+		}
+
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+		showMessage("Google API Connection Suspended");
+	}
+
+	@Override
+	public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+		showMessage("Google API Connection Failed");
+	}
+
 
 	private void showMessage(String message) {
 		Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG);
